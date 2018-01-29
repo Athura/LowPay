bd67fc8c08e9892a863a2b9427a50dd450fb6199
pdjmwj <pdjmwj@live.com>
2018-01-29 09:44:01 -0500

    All .NET framework now

--- VS/Services/RTS/Common/Class1.cs
+++ VS/Services/RTS/Common/Class1.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace RTS.Service.Common2
+namespace RTS.Service.Common
 {
     public class Class1
     {
