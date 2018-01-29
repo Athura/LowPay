ecb9ed266ad9d9140f19f58da380c9dd14cfaadd
pdjmwj <pdjmwj@live.com>
2018-01-29 09:06:30 -0500

    Flipped to .NET Core

--- VS/Services/RTS/Common/Class1.cs
+++ VS/Services/RTS/Common/Class1.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace RTS.Service.Common
+namespace RTS.Service.Common2
 {
     public class Class1
     {
